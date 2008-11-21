7b6618236bd842ba8aa500f84c67e015cb894be3
antonio.s84 <antonio.s84@d6776c9a-ab25-11dd-b08a-b13dd1f1ca2d>
2008-11-21 21:06:14 +0000

    Spostato alcuni metodi in LibrettoControl (altrimenti inutile) Aggiunto getVotoDiLaureaStimato()

---
+++ analisiCarriera/LibrettoControl.java
@@ -0,0 +1,56 @@
+package analisiCarriera;
+import gestionePropriAppelli.RisultatoEsame;
+import struttureDati.iterator.Iterator;
+
+/**
+ * Oggetto control che si occupa di recuperare dati dall'entity Libretto
+ * @author e-vampire
+ */
+public class LibrettoControl {
+
+     /**
+      * Metodo che calcola la media aritmetica per un dato libretto
+      * @param libretto Il libretto per cui si vuol calcolare la media
+      * @return La media aritmetica
+      */
+    public float getMediaAritmetica(Libretto libretto){
+        int sumVoti;
+        sumVoti = 0;
+        RisultatoEsame tmp;
+        for(Iterator<RisultatoEsame> it = libretto.iterator(); it.hasNext();){
+            tmp = it.next();
+            sumVoti+=tmp.getVoto();
+        }
+        return (float)sumVoti/libretto.getNumEsami();
+    }
+    
+    /**
+     * Metodo che calcola la media pesata per un dato libretto
+     * @param libretto Il libretto per cui si vuol calcolare la media
+     * @return La media ponderata
+     */
+    public float getMediaPonderata(Libretto libretto){
+        int sumVotiPesati, sumCrediti;
+        sumVotiPesati = 0;
+        sumCrediti = 1;
+        RisultatoEsame tmp;
+        for(Iterator<RisultatoEsame> it = libretto.iterator(); it.hasNext();)
+        {
+            tmp = it.next();
+            int crediti = tmp.getAppello().getEsame().getCrediti();
+            int votoPesato = tmp.getVoto() * crediti;
+            sumVotiPesati += votoPesato;
+            sumCrediti *= crediti;
+        }
+        return (float)sumVotiPesati/sumCrediti;
+    }
+    
+    /**
+     * Metodo che fa un calcolo del voto di laurea atteso con la media attuale
+     * @param libretto Il libretto contenente gli esami sostenuto
+     * @return Il voto di laurea in centodecimi
+     */
+    public byte getVotoDiLaureaStimato(Libretto libretto){
+        return (byte)((getMediaAritmetica(libretto) * 110) / 30);
+    }
+}
