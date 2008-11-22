53f281e617cf6ba6a3c8c798dd25c439512e6742
antonio.s84 <antonio.s84@d6776c9a-ab25-11dd-b08a-b13dd1f1ca2d>
2008-11-22 19:09:37 +0000

    Sono riuscito ad estendere la classe AbstractTableModel, alla fine è una stupidagine. Per la vostra felicità, le swing seguono l'architettura MVC, La JTable è il view, AbstractTableModel è, appunto il model. Per cui basta far estendere ad ogni entity questa classe astratta per poterle usare all'interno di tabelle senza troppa fatica. Inoltre, ho pensato un pò alle strutture dati da usare. Inutile utilizzare le classi di LASD, va a finire che ci trovano qualche errore. Per quel che ci interessa, Iterator ci stà, Vector ci sta. Utilizziamo quelle e abbiamo un codice più pulito. A domani!

--- trunk/studente/analisiCarriera/Libretto.java
+++ trunk/studente/analisiCarriera/Libretto.java
@@ -1,20 +1,20 @@
 package studente.analisiCarriera;
 
 import docente.gestionePropriAppelli.RisultatoEsame;
-import struttureDati.sequence.NodeSequence;
-import struttureDati.sequence.Sequence;
-import struttureDati.iterator.Iterator;
+import javax.swing.table.AbstractTableModel;
+import java.util.Vector;
+import java.util.Iterator;
 /**
  * Classe che modella il libretto di uno studente.
  * E' una vista dell'entità libretto presente nel database.
  * @author
  */
-public class Libretto {
+public class Libretto extends AbstractTableModel {
 
-    Sequence<RisultatoEsame> listaEsami;
+    Vector<RisultatoEsame> listaEsami;
 
     public Libretto() {
-        listaEsami = new NodeSequence<RisultatoEsame>();
+        listaEsami = new Vector<RisultatoEsame>();
     }
     
     public Iterator<RisultatoEsame> iterator(){ 
@@ -24,6 +24,50 @@ public class Libretto {
     public int getNumEsami(){
         return listaEsami.size();
     }
+
+    public int getColumnCount() {
+        return 4;
+    }
+
+    public int getRowCount() {
+        return listaEsami.size();
+    }
+
+    @Override
+    public Class<?> getColumnClass(int column) {
+        return getValueAt(0,column).getClass();
+    }
+
+    @Override
+    public String getColumnName(int column){
+        switch(column){
+            case 0: return "Esame";
+            case 1: return "Voto";
+            case 2: return "Lode";
+            case 3: return "Data";
+            default: return "";
+        }
+    }
+    
+    public Object getValueAt(int row, int columns) {
+        
+        RisultatoEsame tmp = listaEsami.get(row);
+        
+        switch(columns){
+            case 0: return tmp.getAppello().getEsame().getNome();
+            case 1: return tmp.getVoto();
+            case 2: return tmp.isLode();
+            case 3: return tmp.getAppello().getData();
+            default: return null;
+        }
+    }
+
+    @Override
+    public boolean isCellEditable(int arg0, int arg1) {
+        return false;
+    }
+    
+    
     
     
     
