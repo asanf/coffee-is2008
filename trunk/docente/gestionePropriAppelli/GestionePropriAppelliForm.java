da42d224fb45673a914b1ae6abe8082188581bcc
antonio.s84 <antonio.s84@d6776c9a-ab25-11dd-b08a-b13dd1f1ca2d>
2008-11-26 00:55:59 +0000

    tralalà

--- trunk/docente/gestionePropriAppelli/GestionePropriAppelliForm.java
+++ trunk/docente/gestionePropriAppelli/GestionePropriAppelliForm.java
@@ -1,23 +1,19 @@
 /*
- * HomeDocenteForm.java
+ * GestionePropriAppelliForm.java
  *
- * Created on 26 novembre 2008, 0.35
+ * Created on 26 novembre 2008, 1.44
  */
 
-package docente;
-
-import operatore.gestioneUtenti.Docente;
-import system.HomeForm;
+package docente.gestionePropriAppelli;
 
 /**
  *
- * @author  
+ * @author  e-vampire
  */
-public class HomeDocenteForm extends HomeForm {
+public class GestionePropriAppelliForm extends javax.swing.JFrame {
     
-    /** Creates new form HomeDocenteForm */
-    public HomeDocenteForm(Docente docente) {
-        super(docente);
+    /** Creates new form GestionePropriAppelliForm */
+    public GestionePropriAppelliForm() {
         initComponents();
     }
     
@@ -29,34 +25,34 @@ public class HomeDocenteForm extends HomeForm {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        gestionePropriAppelliButton = new javax.swing.JButton();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        gestionePropriAppelliButton.setText("Gestione Propri Appelli");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(gestionePropriAppelliButton)
-                .addContainerGap(268, Short.MAX_VALUE))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(89, Short.MAX_VALUE)
-                .addComponent(gestionePropriAppelliButton)
-                .addContainerGap())
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GestionePropriAppelliForm().setVisible(true);
+            }
+        });
+    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton gestionePropriAppelliButton;
     // End of variables declaration//GEN-END:variables
     
 }
---
+++ trunk/docente/gestionePropriAppelli/Prenotato.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package docente.gestionePropriAppelli;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author e-vampire
+ */
+public class Prenotato implements Serializable {
+
+}
