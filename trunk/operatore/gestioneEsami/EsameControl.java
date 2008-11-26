084169b788ce80a72a0ace98987c0be0d25fc7e1
antonio.s84 <antonio.s84@d6776c9a-ab25-11dd-b08a-b13dd1f1ca2d>
2008-11-26 10:16:35 +0000

    tralalà - 2

---
+++ trunk/operatore/gestioneEsami/EsameControl.java
@@ -0,0 +1,29 @@
+
+package operatore.gestioneEsami;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author 
+ * 
+ */
+public class EsameControl {
+
+    
+    protected void creaEsame(){
+        
+    }
+    
+    protected void modificaEsame(){
+        
+    }
+    
+    protected void eliminaEsame(){
+        
+    }
+    
+    public Vector<Esame> ricercaEsame(){
+        return null;
+    }
+}
